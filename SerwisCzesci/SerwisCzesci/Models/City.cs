889b110e00bdd94c123c82d8ea2f8d132425d506
Szymon <33634277+michonszy@users.noreply.github.com>
2021-10-18 18:33:10 +0200

    Initial commit

---
+++ SerwisCzesci/SerwisCzesci/Models/City.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class City
+{
+	public City()
+	{
+	}
+}
---
+++ SerwisCzesci/SerwisCzesci/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SerwisCzesci.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
